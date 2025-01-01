0f6d83528e337b500be9f47815f905ef3d237085
91898 <chandansatyendraprasad@gmail.com>
2025-01-01 10:19:31 +0530

    Initial commit

---
+++ src/C/DayZero.java
@@ -0,0 +1,21 @@
+package C;
+
+public class DayZero {
+    public static void main(String[] args) {
+//        Type I
+//        System.out.println("raj");
+//        Type II
+//        String string="RAJ";
+//        System.out.println(string);
+//        Type III
+        String string="RAJ";
+        System.out.println(string);
+        int a=0;
+        System.out.println("Length ="+string.length());
+        a=string.length();
+        int sum=a+118;
+        char aa= (char) sum;
+        System.out.println("Casting ="+aa);
+
+    }
+}
---
+++ src/C/DayZeroK.java
@@ -0,0 +1,13 @@
+package C;
+
+public class DayZeroK {
+    public static void main(String[] args) {
+        int a=35,b=90;
+        float aa=a,bb=b;
+        double aaa=aa+a,bbb=bb+b;
+        System.out.println(aaa + " "+bbb);
+        System.out.println("type conversion="+aa+" and "+bb);
+        char abc = (char) bbb;
+        System.out.println("Type Casting :: "+abc);
+    }
+}
